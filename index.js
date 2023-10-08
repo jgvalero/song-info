39768bce162a31ea08b774adb80dec1f59cc3468
Jose Valero <jvalero000@gmail.com>
2023-10-07 20:30:26 -0700

    Add form functionality

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+	try {
+		res.render("index.ejs");
+	}
+	catch (error) {
+		console.log(error.response.data);
+		res.status(500);
+	}
+});
+
+app.post("/", (req, res) => {
+	try {
+		console.log(req.body);
+		res.render("index.ejs");
+	}
+	catch (error) {
+		console.log(error.response.data);
+		res.status(500);
+	}
+});
+	
 
 app.listen(port, () => {
 	console.log(`Yo, we running this server on port ${port}!`);
