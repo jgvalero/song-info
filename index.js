0e267a639484370f138d14c996bdc96de5348727
Jose Valero <jvalero000@gmail.com>
2023-10-07 18:57:04 -0700

    Add necessary components and packages

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+	console.log(`Yo, we running this server on port ${port}!`);
+});
