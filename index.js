b93f07ed901cef296417186ab63fe8e7ab276780
Jose Valero <jvalero000@gmail.com>
2023-10-12 20:40:33 -0700

    Add Genius API response

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 
+import { readFile } from "fs/promises";
+
 const app = express();
 const port = 3000;
+
+// Get config file
+let config = null;
+try {
+	const filePath = new URL("./config.json", import.meta.url);
+	const contents = await readFile(filePath, {encoding: "utf-8" });
+	config = JSON.parse(contents); 
+	
+} catch (err) {
+	console.error(err.message);
+}
+const GENIUS_URL = "https://api.genius.com/";
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -17,10 +31,16 @@ app.get("/", (req, res) => {
 	}
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
 	try {
-		console.log(req.body);
-		res.render("index.ejs");
+		// console.log(req.body);
+		let artist = req.body["artist"];
+		let song = req.body["song"];
+		res.render("index.ejs", {artist: artist, song: song});
+
+		const result = await axios.get(`${GENIUS_URL}search?q=${artist} ${song}`, { headers: { Authorization: `Bearer ${config["geniusToken"]}` } });
+
+		console.log(result.data.response.hits[0].result.id);
 	}
 	catch (error) {
 		console.log(error.response.data);
